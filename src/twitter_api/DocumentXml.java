b765878cc422cdb4d954308d82c3f74d4c5e828f
Alexandre Croteau-Pothier <alexcp.com@gmail.com>
2013-01-17 21:31:02 -0500

    exemple présenté en classe

---
+++ src/twitter_api/DocumentXml.java
@@ -0,0 +1,34 @@
+package twitter_api;
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+
+public class DocumentXml {
+    private Document document;
+
+    public DocumentXml(String url) throws Exception{
+        this.document = obtenirLeContenuDeLurl(url);
+    }
+
+    public String obtenirTexteDeLElement(Node parent,String nomElement){
+        String resultat = null;
+        NodeList liste = parent.getChildNodes();
+        for(int i=0; i < liste.getLength(); i++){
+            if(liste.item(i).getNodeName().equals(nomElement)){
+                resultat = liste.item(i).getTextContent();
+            }
+        }
+        return resultat;
+    }
+
+    public NodeList obtenirLesElements(String nomDesElements){
+        return document.getElementsByTagName(nomDesElements);
+    }
+
+    private Document obtenirLeContenuDeLurl(String url) throws Exception{
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder  builder = factory.newDocumentBuilder();
+        return builder.parse(url);
+    }
+
+
+}
---
+++ src/twitter_api/Twitter_api.java
@@ -0,0 +1,26 @@
+package twitter_api;
+import org.w3c.dom.*;
+
+public class Twitter_api {
+
+    public static String constuireUrl(String screenName){
+        String url = "https://api.twitter.com/1/statuses/user_timeline.xml?screen_name=";
+        url += screenName;
+        url += "&count=5";
+        return url;
+    }
+
+    public static void main(String[] args) {
+        String url = constuireUrl(args[0]);
+        try{
+            DocumentXml xml = new DocumentXml(url);
+            NodeList liste = xml.obtenirLesElements("status");
+            for(int i=0; i < liste.getLength();i++){
+                System.out.println(xml.obtenirTexteDeLElement(liste.item(i),"text"));
+            }
+
+        }catch(Exception e){
+            System.out.println("Erreur: Impossible de lire l'url");
+        }
+    }
+}
